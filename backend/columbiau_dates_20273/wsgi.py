0d9832a40f5855a79a9088236f935e15ff67d880
Crowdbotics <team@crowdbotics.com>
2020-09-15 00:37:43 +0000

    Update models

--- backend/columbiau_dates_20273/wsgi.py
+++ backend/columbiau_dates_20273/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'columbiau_dates_20273.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "columbiau_dates_20273.settings")
 
 application = get_wsgi_application()
